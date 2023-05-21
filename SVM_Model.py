e6f7e630086da7ebdaed3a4f6fac42b093d24989
SMS-Sajadi <soroush.s2017@gmail.com>
2023-05-21 13:41:05 +0330

    Better output for user

--- SVM_Model.py
+++ SVM_Model.py
@@ -17,19 +17,24 @@ from Modules.Train_Test_Split import data_split
 def main():
     # Loading images and their labels
     images, labels = data_loader(GENKI_IMAGE_ROOT, GENKI_LABEL_ROOT)
+    print("|----------------------- Dataset Loaded -----------------------|\n")
 
     # Detecting face in the images
     images = face_detector(images)
+    print("|----------------------- Face Detection Done -----------------------|\n")
 
     # Feature Extracting
     feature_matrix = feature_extract(images)
+    print("|----------------------- Features Extracted -----------------------|\n")
 
     # Splitting the image's features
     train_features, train_labels, test_features, test_labels = data_split(feature_matrix, labels, 0.3, shuffling=True)
+    print("|----------------------- Dataset Split Done -----------------------|\n")
 
     # Training the SVM
     svm_model = make_pipeline(StandardScaler(), SVC(kernel="rbf", C=1.0))
     svm_model.fit(train_features, train_labels)
+    print("|----------------------- Model Trained -----------------------|\n")
 
     # Finding the train accuracy
     print("|----------------------- Training Result -----------------------|")
