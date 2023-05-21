8b865956787d0ab454d7396932571455433e19ef
SMS-Sajadi <soroush.s2017@gmail.com>
2023-05-21 15:11:36 +0330

    fixing the face detector function for single image work

--- Modules/Face_Detector.py
+++ Modules/Face_Detector.py
@@ -30,15 +30,19 @@ def face_detector(images):
     return faces
 
 
-def face_detector_single(image):
+def face_detector_single(img):
     """
     This function will crop the face in the image and return the list of them
-    :param image:
-    :return faces:
+    :param img:
+    :return faces, loc:
     """
 
-    # A list for saving the faces
+    # Changing image into gray scale
+    image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # A list for saving the faces and their locations
     faces = []
+    loc = []
 
     # Creating the face detector obj
     face_detector = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
@@ -47,8 +51,11 @@ def face_detector_single(image):
     face_detected = face_detector.detectMultiScale(image, 1.1, 4)
 
     for (x, y, w, h) in face_detected:
+        # Location of face
+        loc.append((x,y,w,h))
+
         # Save the detected face
         face_crop = image[y: y + h, x: x + w]
         faces.append(cv2.resize(face_crop, (64,64)))
 
-    return faces
+    return faces, loc
