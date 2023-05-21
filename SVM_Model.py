1e28258650fef51880d872dfd1f7218e7875a0d6
SMS-Sajadi <soroush.s2017@gmail.com>
2023-05-21 13:36:51 +0330

    Adding save option for model

--- SVM_Model.py
+++ SVM_Model.py
@@ -35,14 +35,29 @@ def main():
     print("|----------------------- Training Result -----------------------|")
     train_output = svm_model.predict(train_features)
     train_accuracy = accuracy_score(train_output, train_labels)
-    print(f"Accuracy: %{train_accuracy*100} on {len(train_features)} train data")
+    print(f"Accuracy: %{train_accuracy*100} on {len(train_features)} train data\n\n")
 
     # Testing the Model
     print("|----------------------- Testing Result -----------------------|")
     test_output = svm_model.predict(test_features)
     test_accuracy = accuracy_score(test_output, test_labels)
-    print(f"Accuracy: %{test_accuracy*100} on {len(test_features)} train data")
+    print(f"Accuracy: %{test_accuracy*100} on {len(test_features)} train data\n\n")
 
+    # Ask for saving
+    user_entry = input("Do you want to save this model?(y,N): ")
+    if user_entry == "y":
+        # Saving the Model
+        with open("Model/SVM_Trained.pkl", "wb") as f:
+            pickle.dump(svm_model, f)
+
+        print("-- Model Saved Successfully in ./Model/SVM_Trained.pkl --")
+
+    elif user_entry == "N":
+        # Ignoring the trained Model
+        print("--- Model didn't saved ---")
+    return
+
+N
 if __name__ == "__main__":
     # Setting Genki dataset address
     GENKI_IMAGE_ROOT = "../Genki Dataset/genki4k/files/"
