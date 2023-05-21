2064a7e31a4e7cac53e2f94d6b6199b4e628d1a9
SMS-Sajadi <soroush.s2017@gmail.com>
2023-05-21 20:38:13 +0330

    some new edits for clean coding

--- Smile_Detector.py
+++ Smile_Detector.py
@@ -6,15 +6,15 @@ import pickle
 from Modules.Face_Detector import face_detector_single
 from Modules.Feature_Extractor import feature_extract_single
 
+
 def main():
     # Loading the trained model
     with open("./Model/SVM_Trained.pkl", "rb") as f:
         svm_model = pickle.load(f)
         print("-- Model loaded Successfully --")
 
-    # Accessing Camera and writing on it
-    capture = cv2.VideoCapture("20230521_142847.mp4")
-    #video_writer = cv2.VideoWriter()
+    # Accessing Source and writing on it
+    capture = cv2.VideoCapture(capture_source)
 
     # Reading the Camera
     while(True):
@@ -42,12 +42,14 @@ def main():
 
                 # if smile predicted
                 if output[0] == '1':
-                    print(1)
+                    # It will show that smile is detected
+                    print(f"Face {i} has smile!")
 
                     # Color will be Green
                     color = (0, 255, 0)
                 else:
-                    print(0)
+                    # It will show that there is no smile
+                    print(f"Face {i} has no smile!")
 
                     # if smile is not detected, color will be red
                     color = (0, 0, 255)
@@ -57,14 +59,16 @@ def main():
 
             # Showing the frame with rectangle
             cv2.imshow("Video", frame)
-            cv2.waitKey(5)
+            cv2.waitKey(2)
 
         else:
             # Frame is not read
-            print("-- Frame is not loaded successfully --")
+            print("-- Frame is not loaded successfully or source is ended --")
             break
 
 
 if __name__ == "__main__":
+    # Source for capturing frames from
+    capture_source = "20230521_142847.mp4"
 
     main()
