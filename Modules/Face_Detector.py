419c2310dc8e0e53dd225ca58e795933b2d1a7f9
SMS-Sajadi <soroush.s2017@gmail.com>
2023-05-21 14:46:26 +0330

    Adding the functions for single pic

--- Modules/Face_Detector.py
+++ Modules/Face_Detector.py
@@ -4,7 +4,7 @@ import cv2
 
 def face_detector(images):
     """
-    This function will crop the face in the image and return the list of them
+    This function will crop the faces in the images and return the list of them
     :param images:
     :return faces:
     """
@@ -25,6 +25,30 @@ def face_detector(images):
 
         # Save the detected face
         face_crop = img[y: y + h, x: x + w]
+        faces.append(cv2.resize(face_crop, (64,64)))
+
+    return faces
+
+
+def face_detector_single(image):
+    """
+    This function will crop the face in the image and return the list of them
+    :param image:
+    :return faces:
+    """
+
+    # A list for saving the faces
+    faces = []
+
+    # Creating the face detector obj
+    face_detector = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+
+    # Detecting face in image
+    face_detected = face_detector.detectMultiScale(image, 1.1, 4)
+
+    for (x, y, w, h) in face_detected:
+        # Save the detected face
+        face_crop = image[y: y + h, x: x + w]
         faces.append(cv2.resize(face_crop, (64,64)))
 
     return faces
--- Modules/Feature_Extractor.py
+++ Modules/Feature_Extractor.py
@@ -34,3 +34,33 @@ def feature_extract(images, histogram_feature=False):
     image_features = np.array(image_features)
 
     return image_features
+
+
+def feature_extract_single(image, histogram_feature=False):
+    """
+    This function will extract hog and lbp feature of image and return the feature matrix
+    histogram_feature is a flag that will decide the lbp or its histogram will bre returned
+    :param image:
+    :param histogram_feature:
+    :return image_features matrix:
+    """
+    # A list for saving features
+    image_features = []
+    radius = 3
+    n_points = 8 * radius
+
+    # Extracting HOG of image
+    hog_features = hog(image, orientations=16, pixels_per_cell=(16, 16), cells_per_block=(1, 1), visualize=False,
+                       feature_vector=True)
+    # Extracting LBP of image
+    lbp_features = local_binary_pattern(image, n_points, radius, method='default')
+    # Creating the histogram of the lbp
+    if histogram_feature:
+        lbp_features, _ = np.histogram(lbp_features.ravel(), bins=np.arange(0, n_points + 3), range=(0, n_points + 2))
+    # Adding features to the matrix
+    image_features.append(np.concatenate((hog_features, lbp_features.flatten())))
+
+    # Change list to numpy array
+    image_features = np.array(image_features)
+
+    return image_features
